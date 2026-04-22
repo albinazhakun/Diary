81ad32dee350015a75e1e04f4bae7b54be5248c4
ZhakunAlbina <albina.zhakun@nure.ua>
2026-04-22 23:16:22 +0300

    added classes

---
+++ EntityClass/Category.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Diary.EntityClass
+{
+    public class Category
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public string Name { get; set; } = string.Empty;
+        public string? Description { get; set; }
+        public string? Color { get; set; } = "#FFFFFF";
+    }
+}
---
+++ EntityClass/Event.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Diary.EntityClass
+{
+    public class Event
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public string Title { get; set; } = string.Empty;
+        public DateOnly Date { get; set; }
+        public TimeOnly Time { get; set; }
+        public int Duration { get; set; }
+        public string? Location { get; set; }
+        public Prior Priority { get; set; }
+        public Guid? CategoryId { get; set; }
+        public bool RemindShow { get; set; } = false;
+        public TimeOnly EndTime => Time.AddMinutes(Duration);
+        public bool IsPast =>
+            Date < DateOnly.FromDateTime(DateTime.Now) ||
+            (Date == DateOnly.FromDateTime(DateTime.Now) && EndTime < TimeOnly.FromDateTime(DateTime.Now));
+        
+    }
+    public enum Prior
+    {
+        Low,
+        Medium,
+        High 
+    }
+}
